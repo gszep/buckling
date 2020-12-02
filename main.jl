3b685bc49354cf9e7cbdd8b632826af75453fcf7
gszep <gregory.szep@gmail.com>
2020-12-02 00:45:04 +0000

    plot circle on top of data

--- main.jl
+++ main.jl
@@ -5,11 +5,16 @@ M = binarize(load("data/E2/M.tif"), Otsu())
 M = opening(opening(M))
 
 begin
-    t = slider(1:size(M,3),sliderlength=400,position=(50,0),start=26)
-    Mₜ = lift(t[end].value) do i M[:,:,i] end
+    t = slider(1:size(M,3),sliderlength=700,position=(50,0),start=26,valueprinter=x->"$(round(15*x/60,digits=1)) min")
+    Mₜ = lift(t[end].value) do i .~M[:,:,i] end
 
-    RecordEvents( hbox( image(Mₜ,axes=false), vbox(t), 
-        parent = Scene(resolution=(500, 500))),
+    Figure = image(range(-1,1,length=size(M,1)),range(-1,1,length=size(M,2)),Mₜ,show_axis=false)
+
+    θ = range(-π,π,length=100)
+    lines!(3cos.(θ)/4,3sin.(θ)/4,color=:gold,linewidth=3)
+
+    RecordEvents( hbox( Figure, vbox(t), 
+        parent = Scene(resolution=(800, 800))),
     "output" )
 end
 
