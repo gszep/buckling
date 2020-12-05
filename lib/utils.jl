c17bcdd3725e1940f7c99586c8492623e5f063f8
gszep <gregory.szep@gmail.com>
2020-12-05 13:46:02 +0100

    rendering original image

--- lib/utils.jl
+++ lib/utils.jl
@@ -11,40 +11,36 @@ const μm = Unitful.μm
 
 function File(path::String; t=:, x=:, y=:, z=:, Δt=15s, Δr=(0.37μm,0.37μm,1.5μm) )
     filename,ext = splitext(path)
-    Δx, Δy, Δz = Δr
+    c = 1 # number of channels
 
     if occursin("h5",ext)
         A = h5read(path,"data",(:,x,y,z,t))
         @assert((size(A,1)==1)&(ndims(A)==5),"allowed formats: single channel TIF (x,y,t) or HDF5 (x,y,z,t)")
-
-        images = A[1,:,:,:,:]
-        return AxisArray(images,
-            Axis{:x}( size(images,1)>1 ? range(-Δx*size(images,1)/2,Δx*size(images,1)/2,length=size(images,1)) : 0*Δx ),
-            Axis{:y}( size(images,2)>1 ? range(-Δy*size(images,2)/2,Δy*size(images,2)/2,length=size(images,2)) : 0*Δy ),
-            Axis{:z}( size(images,3)>1 ? range(-Δz*size(images,3)/2,Δz*size(images,3)/2,length=size(images,3)) : 0*Δz ),
-            Axis{:t}( size(images,4)>1 ? range(0*Δt,Δt*size(images,4),length=size(images,4)) : 0*Δt ),
-        )
+        z = 1:size(A,4)
 
     elseif occursin("tif",ext)
-        A = Gray.(load(path))
-        @assert((ndims(A)==3),"allowed formats: single channel TIF (x,y,t) or HDF5 (x,y,z,t)")
-        
-        newaxis = [CartesianIndex()]
-        images = rawview(channelview(A))[:,:,newaxis,:]
+        A = channelview(load(path))
+        @assert(ndims(A)==4,"allowed formats: single channel TIF (x,y,t) or HDF5 (x,y,z,t)")
+        z = [CartesianIndex()] # single z-slice
 
-        return AxisArray(images,
-            Axis{:x}( size(images,1)>1 ? range(-Δx*size(images,1)/2,Δx*size(images,1)/2,length=size(images,1)) : 0*Δx ),
-            Axis{:y}( size(images,2)>1 ? range(-Δy*size(images,2)/2,Δy*size(images,2)/2,length=size(images,2)) : 0*Δy ),
-            Axis{:z}( size(images,3)>1 ? range(-Δz*size(images,3)/2,Δz*size(images,3)/2,length=size(images,3)) : 0*Δz ),
-            Axis{:t}( size(images,4)>1 ? range(0*Δt,Δt*size(images,4),length=size(images,4)) : 0*Δt ),
-        )
     else 
         throw("allowed formats: single channel TIF (x,y,t) or HDF5 (x,y,z,t)")
     end
+
+    Δx, Δy, Δz = Δr
+    images = A[c,:,:,z,:]
+
+    return AxisArray( colorview(Gray,normedview(images)),
+
+        Axis{:x}( size(images,1)>1 ? range(-Δx*size(images,1)/2,Δx*size(images,1)/2,length=size(images,1)) : 0*Δx ),
+        Axis{:y}( size(images,2)>1 ? range(-Δy*size(images,2)/2,Δy*size(images,2)/2,length=size(images,2)) : 0*Δy ),
+        Axis{:z}( size(images,3)>1 ? range(-Δz*size(images,3)/2,Δz*size(images,3)/2,length=size(images,3)) : 0*Δz ),
+        Axis{:t}( size(images,4)>1 ? range(0*Δt,Δt*size(images,4),length=size(images,4)) : 0*Δt )
+    )
 end
 
 
-function targets( A::AxisArray{<:Bool}; downSample::Integer=10 )
+function targets( A::AxisArray{<:Bool}; downSample::Integer=1 )
     targets = Vector{SVector{ndims(A),Float64}}()
 
     for index ∈ findall(A)[1:downSample:end]
--- main.jl
+++ main.jl
@@ -12,12 +12,12 @@ begin #################################### required libs
 end
 
 begin #################################### load and binarize image
-    name = "E2/M.h5"
-    images = File(joinpath("data",name), z=48:52, Δr=[0.37μm,0.37μm,1.5μm], Δt=15s)
+    name = "E2/M.tif"
+    images = File(joinpath("data",name), z=40:60, Δr=[0.37μm,0.37μm,1.5μm], Δt=15s)
 
     binaryImages = AxisArray( Array{Bool}(undef,size(images)), images.axes )
     binarize!(binaryImages,images,Otsu())
-    region = Tuple(1:ndims(images)-1)
+    region = Tuple(1:ndims(images))
 
     #################################### de-noise binarization
     opening!(binaryImages,region); closing!(binaryImages,region) # convert to sparse(binaryImages) ?
@@ -27,24 +27,27 @@ end
 begin ################################################################## display image timeseries
     scene, layout = layoutscene(resolution = (800, 800))
     tSlider = layout[1,1] = LSlider( scene, range=1:nimages(images), startvalue=26 )
-    ax = layout[2,1] = LScene(scene, camera = cam3d!, raw = false, xlabel = "μm", ylabel = "μm")
+    ax = layout[2,1] = LScene(scene, camera = cam3d!)
 
-    ######################### scatter binarized image
-    dataₜ = lift(tSlider.value) do t map( x-> Point(x[1],x[2],x[3]), targets(binaryImages[Axis{:t}(t)]) ) end
-    meshscatter!( ax, dataₜ, color = :darkblue, markersize = 1 )
+    ######################### show image volume
+    imageₜ = lift(tSlider.value) do t images[Axis{:t}(t)].data end
+    x,y,z,_ = ( range( extrema(ustrip(ax.val))..., length=2) for ax ∈ images.axes )
+    volume!( ax, x,y, minimum(z) ≠ maximum(z) ? z : range(-1,1,length=2), imageₜ)
 
     ######################### boundary model
     angles = range(-π,π,length=500)
-    weights = randn(50)
-
-    boundary_positions = hcat( boundary.(angles; weights=weights*μm, radius=75.0μm, origin=zeros(2)μm )... )'
-    lines!( ax, ustrip(uconvert.(μm,boundary_positions)), linewidth=3, color=:gold )
+    boundary_positions = hcat( boundary.(angles; weights=weights, radius=75.0, origin=[0.0,5] )... )'
+    lines!( ax, boundary_positions, linewidth=3, color=:gold )
 
     ######################### render all
     RecordEvents( scene, "output" )
 end
 
 data = targets(binaryImages[Axis{:z}(1),Axis{:t}(26)])
+using Flux: Momentum,update!
 
-loss(data;weights=weights)
-∇loss(data;weights=weights)
+optimiser = Momentum(1.0)
+for i ∈ 1:100
+    gradients = ∇loss(data;weights=weights,radius=75.0,origin=[0.0,5])
+    update!(optimiser,weights,gradients)
+end
